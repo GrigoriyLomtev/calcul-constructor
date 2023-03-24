e44df8be0b6f0888d5fa17fde93159d62a8df24c
GrigoriyLomtev <grisha2198@gmail.com>
2023-03-24 21:59:18 +0300

    feat: add input logic

--- src/components/common/InputField.tsx
+++ src/components/common/InputField.tsx
@@ -1,23 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useTypedSelector } from '../../hooks/useTypedSelector';
 import { borderRadiusElement, boxShadowElement, marginBottomElement } from '../../styles/constants';
 
+const inputFieldStyle = {
+  height: '60px',
+  width: '240px',
+  display: 'flex',
+  justifyContent: 'flex-end',
+  fontWeight: '800',
+  padding: '10px 12px',
+  marginBottom: marginBottomElement,
+  boxShadow: boxShadowElement,
+  borderRadius: borderRadiusElement,
+};
+
 function InputField() {
+  const [value, setValue] = useState('0');
+  const { firstValue, isGetFirstValue, secondValue } = useTypedSelector((state) => state.calculation);
+
+  const fontSize = (value: string) => {
+    if (value.length >= 9 && value.length <= 14) return '24px';
+    if (value.length >= 15) return '19px';
+    return '36px';
+  };
+
+  useEffect(() => {
+    setValue(isGetFirstValue ? secondValue : firstValue);
+  }, [firstValue, isGetFirstValue, secondValue]);
+
   return (
-    <div
-      css={{
-        height: '60px',
-        width: '240px',
-        display: 'flex',
-        justifyContent: 'flex-end',
-        fontSize: '36px',
-        fontWeight: '800',
-        padding: '10px 12px',
-        marginBottom: marginBottomElement,
-        boxShadow: boxShadowElement,
-        borderRadius: borderRadiusElement,
-      }}
-    >
-      {0}
+    <div css={{ ...inputFieldStyle, fontSize: fontSize(value) }}>
+      <p
+        css={{
+          marginTop: 'auto',
+        }}
+      >
+        {value}
+      </p>
     </div>
   );
 }
